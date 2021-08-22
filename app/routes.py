c5a3fea4cc21fae55ab04593fbd1a68371e9b12a
Tyler Carbone <tcarbone073@gmail.com>
2021-08-22 09:06:11 -0400

    Showing logged in user templates

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,7 @@ def index():
             "body": "The avengers movie was so cool!"
         }
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
