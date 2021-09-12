f80ef5b4803c81a6e0d85093a1514b258283386d
Tyler Carbone <tcarbone@CentOS-Stream8-VB.home>
2021-09-12 16:57:04 -0400

    Send errors by email

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 
 # Initialize the application and load configuration settings from the
@@ -38,4 +40,30 @@ login.login_view = "login"
 # This is because the `routes` module imports the `app` variable defined above.
 # This avoids a circular import.
 from app import routes, models, errors
+
+# Configurations for production
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+            
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost = (app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr = "no-reply@" + app.config["MAIL_SERVER"],
+            toaddrs = app.config["ADMINS"],
+            subject = "Microblog Failure",
+            credentials = auth,
+            secure = secure
+        )
+        
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
 
